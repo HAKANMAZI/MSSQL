8991d447ec7650c544e8614008342d57f616dda9
HAKANMAZI <hak122hak@gmail.com>
2019-08-27 17:57:11 +0300

    remove all non numeric values

--- functions/Remove Chars and get only numeric values.sql
+++ functions/Remove Chars and get only numeric values.sql
@@ -9,7 +9,8 @@ set @total = len(@phone)
 while @count <= @total
 begin
 	set @newValue = SUBSTRING(@phone,@count,1)
-	if ISNUMERIC(@newValue) = 1
+	--if ISNUMERIC(@newValue) = 1   /* this function doesnt clean ., characters */
+	if @newValue in ('0','1','2','3','4','5','6','7','8','9')
 	begin
 		set @combine = @combine + @newValue
 	end
