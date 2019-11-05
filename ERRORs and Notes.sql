ce096185f9bedaef7ec092cee64770713a08c14b
HAKANMAZI <hak122hak@gmail.com>
2019-11-05 13:47:08 +0300

    Create ERRORs and Notes.sql

---
+++ ERRORs and Notes.sql
@@ -0,0 +1,3 @@
+-- (Error converting data type nvarchar to decimal.) 
+-- not: Rakam haricinde bir değer olmadığından eminseniz o zaman sorun virgül ile alakalı olabilir
+cast( replace(column,',','.') as decimal )
