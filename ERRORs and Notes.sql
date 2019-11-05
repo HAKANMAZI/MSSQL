beaf1832cbe99bb12fb9e4197c9c890c83418cec
HAKANMAZI <hak122hak@gmail.com>
2019-11-05 13:50:03 +0300

    Update ERRORs and Notes.sql

--- ERRORs and Notes.sql
+++ ERRORs and Notes.sql
@@ -1,3 +1,8 @@
 -- (Error converting data type nvarchar to decimal.) 
 -- not: Rakam haricinde bir değer olmadığından eminseniz o zaman sorun virgül ile alakalı olabilir
 cast( replace(column,',','.') as decimal )
+
+-- Update tabloyu select query kullanarak
+update table1 
+set = ( select column from table2 
+        where table1.id = table2.id )
