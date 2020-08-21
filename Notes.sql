ab13ec5af7a657a48109c66a24c0ce2c9132162c
HAKANMAZI <hak122hak@gmail.com>
2020-08-21 10:12:23 +0300

    Update Notes.sql

--- Notes.sql
+++ Notes.sql
@@ -70,3 +70,20 @@ set statistics io on
 set statistics time on 
 SELECT * FROM [NORTHWND].[dbo].[Orders] where ShipAddress = 'Luisenstr. 48'
 
+-------------------------- Check for the backup --------------------------
+SELECT 
+ b.database_name,
+    key_algorithm,
+    encryptor_thumbprint,
+    encryptor_type,
+	b.media_set_id,
+    is_encrypted, 
+	type,
+    is_compressed,
+	bf.physical_device_name
+	 FROM msdb.dbo.backupset b
+INNER JOIN msdb.dbo.backupmediaset m ON b.media_set_id = m.media_set_id
+INNER JOIN msdb.dbo.backupmediafamily bf on bf.media_set_id=b.media_set_id
+WHERE database_name = 'NORTHWND'
+ORDER BY b.backup_start_date  DESC
+
