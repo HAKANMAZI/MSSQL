1354f2353233cbf2c291fed5d02b413bcf1c6c68
HAKANMAZI <hak122hak@gmail.com>
2020-10-31 17:22:09 +0300

    Create alwayson.sql

---
+++ alwayson.sql
@@ -0,0 +1,7 @@
+1) Windows on install for DC, Node1, Node2 using VmWare Workstation
+2) Install "Active Directory Domain Services" for DC
+    Create new forest 
+    change Computer Name to DC
+    to ping from DC to Node1,Node2
+    close the firewall for Node1 and Node2. connection between
+3) install Failover Cluster for Node1, Node2
