026ae1f7ab00091e9b373e19c83f46c21ddb7919
HAKANMAZI <hak122hak@gmail.com>
2021-12-04 18:14:48 +0300

    Update Notes.sql

--- Notes.sql
+++ Notes.sql
@@ -150,3 +150,82 @@ begin
 	set @count = @count + 1
 end 
 print @combine
+
+
+
+-- ExtendentEvents
+------------------------------------ ringBuffer1 ------------------------------------
+SELECT CAST(target_data as xml) AS targetdata
+INTO #capture_waits_data
+FROM sys.dm_xe_session_targets xet
+JOIN sys.dm_xe_sessions xes
+    ON xes.address = xet.event_session_address
+WHERE xes.name = 'sql_control'
+  AND xet.target_name = 'ring_buffer';
+ 
+
+insert into #capture_waits_data
+SELECT CAST(target_data as xml) AS targetdata
+FROM sys.dm_xe_session_targets xet
+JOIN sys.dm_xe_sessions xes
+    ON xes.address = xet.event_session_address
+WHERE xes.name = 'sql_control'
+  AND xet.target_name = 'ring_buffer';
+
+SELECT *
+FROM #capture_waits_data;
+--drop table #capture_waits_data
+
+SELECT distinct
+  xed.event_data.value('(@timestamp)[1]', 'datetime2') AS [timestamp],
+  xed.event_data.value('(@name)[1]', 'varchar(max)') AS eventname,
+  xed.event_data.value('(data[@name="database_name"]/value)[1]', 'varchar(200)') AS database_name,
+  xed.event_data.value('(action[@name="username"]/value)[1]', 'varchar(max)') AS username,
+  xed.event_data.value('(action[@name="sql_text"]/value)[1]', 'varchar(max)') AS sql_text,
+  xed.event_data.value('(action[@name="session_nt_username"]/value)[1]', 'varchar(max)') AS session_nt_username,
+  xed.event_data.value('(action[@name="client_app_name"]/value)[1]', 'varchar(max)') AS client_app_name,
+  xed.event_data.value('(action[@name="server_instance_name"]/value)[1]', 'varchar(max)') AS server_instance_name
+FROM #capture_waits_data
+  CROSS APPLY targetdata.nodes('//RingBufferTarget/event') AS xed (event_data);
+  
+------------------------------------ ringBuffer2 ------------------------------------
+WITH hadr(record) AS  
+(  
+SELECT CAST(target_data as xml) AS targetdata
+FROM sys.dm_xe_session_targets xet
+JOIN sys.dm_xe_sessions xes
+    ON xes.address = xet.event_session_address
+WHERE xes.name = 'sql_control'
+  AND xet.target_name = 'ring_buffer'
+)  
+SELECT   
+   record.value('(./RingBufferTarget/event/@name)[1]', 'varchar(100)') AS name,  
+  record.value('(./RingBufferTarget/event/@timestamp)[1]', 'datetime2') AS timestamp, 
+  record.value('(./RingBufferTarget/event/data[@name="database_name"])[1]', 'varchar(100)') AS database_name, 
+  record.value('(./RingBufferTarget/event/action[@name="sql_text"])[1]', 'varchar(max)') AS sql_text,
+  record.value('(./RingBufferTarget/event/action[@name="username"])[1]', 'varchar(max)') AS username
+FROM hadr  
+
+
+
+------------------------------------ ringBuffer3 ------------------------------------
+select 
+  xed.event_data.value('(@timestamp)[1]', 'datetime2') AS [timestamp],
+  xed.event_data.value('(@name)[1]', 'varchar(max)') AS eventname,
+  xed.event_data.value('(data[@name="database_name"]/value)[1]', 'varchar(200)') AS database_name,
+  xed.event_data.value('(action[@name="username"]/value)[1]', 'varchar(max)') AS username,
+  xed.event_data.value('(action[@name="sql_text"]/value)[1]', 'varchar(max)') AS sql_text,
+  xed.event_data.value('(action[@name="session_nt_username"]/value)[1]', 'varchar(max)') AS session_nt_username,
+  xed.event_data.value('(action[@name="client_app_name"]/value)[1]', 'varchar(max)') AS client_app_name,
+  xed.event_data.value('(action[@name="server_instance_name"]/value)[1]', 'varchar(max)') AS server_instance_name
+
+from (
+	SELECT 
+		CAST(target_data as xml) AS targetdata
+	FROM sys.dm_xe_session_targets xet
+	JOIN sys.dm_xe_sessions xes
+	ON xes.address = xet.event_session_address
+	WHERE xes.name = 'sql_control' AND xet.target_name = 'ring_buffer'
+  ) x 
+CROSS APPLY targetdata.nodes('//RingBufferTarget/event') AS xed (event_data)
+
