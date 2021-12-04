1b3046f702000fe4f2abec509f57a008bfb20fd2
HAKANMAZI <hak122hak@gmail.com>
2021-12-04 18:17:41 +0300

    Delete Call_StoreProcedure_UsingLinkedServer.sql

--- Call_StoreProcedure_UsingLinkedServer.sql
+++
@@ -1,4 +0,0 @@
-1)Create LinkedName and change RCP and RCP OUT (True) in LinkedName Properties
-2)exec ('exec DatabasName.schema.StoreProcedureName') at [LinkedName]
-  or
-  exec ('DatabasName.schema.StoreProcedureName') at [LinkedName]
