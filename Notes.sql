538341f5ea44aca126166a09f21c0fb49aae7382
HAKANMAZI <hak122hak@gmail.com>
2021-12-04 18:22:42 +0300

    Update Notes.sql

--- Notes.sql
+++ Notes.sql
@@ -124,6 +124,19 @@ alter database [DatabaseName]
 set multi_user with rollback immediate;
 go
 
+------------------------------- Delete Trigger -------------------------------
+--SQL SERVER – Fix : Error : 17892 Logon failed for login due to trigger execution. Changed database context to ‘master’.
+FirstWay
+  USE master
+  GO
+  DROP TRIGGER [TriggerName] ON ALL SERVER
+  GO
+SecondWay
+  C:\Users\Pinal>sqlcmd -S LocalHost -d master -A
+  1> DROP TRIGGER Tr_ServerLogon ON ALL SERVER
+  2> GO
+
+
 
 
 ---------------------------- users vs logins ----------------------------
