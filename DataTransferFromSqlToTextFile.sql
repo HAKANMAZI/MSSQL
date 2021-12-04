b5c78b230b041fa90a93a918260f932a54a575a5
HAKANMAZI <hak122hak@gmail.com>
2021-12-04 18:19:20 +0300

    Delete DataTransferFromSqlToTextFile.sql

--- DataTransferFromSqlToTextFile.sql
+++
@@ -1,8 +0,0 @@
---Data Transfer From Sql Server To Text File Using BCP format...
-
-declare @output nvarchar(1000), @filepath nvarchar(1000), @bcp nvarchar(1000)
-set @bcp = 'bcp "select * from sys.traces" queryout '
-set @filepath = 'C:\x\'
-set @output = 'text.txt'
-set @bcp = @bcp + @filepath + @output + ' -c -t, -T -S'+ @@SERVERNAME
-EXEC master..xp_cmdshell @bcp
